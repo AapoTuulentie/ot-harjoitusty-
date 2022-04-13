d4696b6f1797a349e0556ac93570ed1e23bc1bcd
Aapo Tuulentie <aapo.tuulentie@helsinki.fi>
2022-04-13 21:40:01 +0300

    Catching up

--- src/index.py
+++ src/index.py
@@ -11,7 +11,7 @@ def main():
     pygame.display.set_caption("Snake game")
     pygame.display.update()
 
-    level = Level()
+    level = Level(display)
     gameloop = GameLoop(display, level)
 
     pygame.init()
