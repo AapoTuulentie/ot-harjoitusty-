dda121906b9aa20daa5b752375d38a5b83174bcc
Aapo Tuulentie <aapo.tuulentie@helsinki.fi>
2022-04-25 18:51:21 +0300

    Quick fixes

--- src/tests/level_test.py
+++ src/tests/level_test.py
@@ -1,5 +1,6 @@
 import unittest
 from level import Level
+import pygame
 
 
 class TestSnake(unittest.TestCase):
@@ -7,8 +8,12 @@ class TestSnake(unittest.TestCase):
     def setUp(self):
 
         self.level = Level()
+        pygame.init()
 
     def test_collisions(self):
 
         self.level.snake_head = [1001, 801]
         self.assertEqual(self.level.check_collisions(), True)
+
+        
+
