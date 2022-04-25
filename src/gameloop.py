4e936cf827addca1ca9b1c5ec282450bcd21e3b4
Aapo Tuulentie <aapo.tuulentie@helsinki.fi>
2022-04-25 16:43:27 +0300

    Quick fixes

--- src/gameloop.py
+++ src/gameloop.py
@@ -8,7 +8,7 @@ class GameLoop:
         self._clock = pygame.time.Clock()
         self.fps = 18
         self._level = level
-        self.direction = "RIGHT"
+        
 
     def start(self):
 
@@ -29,7 +29,7 @@ class GameLoop:
     
     def events(self):
 
-        previous_direction = self.direction
+        previous_direction = self._level.direction
 
         for event in pygame.event.get():
 
@@ -41,19 +41,19 @@ class GameLoop:
 
                 if event.key == pygame.K_UP and previous_direction != "DOWN":
 
-                    self.direction = "UP"
+                    self._level.direction = "UP"
 
                 elif event.key == pygame.K_DOWN and previous_direction != "UP":
 
-                    self.direction = "DOWN"
+                    self._level.direction = "DOWN"
 
                 elif event.key == pygame.K_RIGHT and previous_direction != "LEFT":
 
-                    self.direction = "RIGHT"
+                    self._level.direction = "RIGHT"
 
                 elif event.key == pygame.K_LEFT and previous_direction != "RIGHT":
 
-                    self.direction = "LEFT"
+                    self._level.direction = "LEFT"
 
 
         self._level.move_snake()
