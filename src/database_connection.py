1300e0c169bc1aa5d3a8888de8b02d69155b617e
Tuulentie Aapo K M <aapotuul@cubbli20-gold.local>
2022-05-15 23:42:49 +0300

    build.py and .env

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,18 +1,9 @@
 import os
 import sqlite3
-from dotenv import load_dotenv
 
 dirname = os.path.dirname(__file__)
 
 connection = sqlite3.connect(os.path.join(dirname, "..", "data", "highscores.sqlite"))
-connection.row_factory = sqlite3.Row
-
-try:
-    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
-except FileNotFoundError:
-    pass
-connection = sqlite3.connect(os.path.join(
-    dirname, "..", "data", os.getenv("DATABASE_FILENAME") or "default highscores.sqlite"))
 connection.row_factory = sqlite3.Row
 
 def get_database_connection():
